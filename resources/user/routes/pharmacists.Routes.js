f80b984caf25905b7b1b922fa1d354b6a84be3ad
unknown <fredrickbolutife@gmail.com>
2024-06-21 19:03:08 +0100

    added changes to some roles

--- resources/user/routes/pharmacists.Routes.js
+++ resources/user/routes/pharmacists.Routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../../../middleware/isAuthenticated.js';
-import { addMedicalHistory, createPharmacist, deletePharmacist, getAllActivePatients, getAllPharmacists, getMedicationHistory, getPatientsPrescription, getPharmacistByName, loginPharmacist, updatePharmacist } from '../controllers/pharmacists.Controller.js';
+import { addMedicalHistory, addMedicine, createPharmacist, deletePharmacist, getAllActivePatients, getAllPharmacists, getMedicationHistory, getPatientsPrescription, getPharmacistByName, loginPharmacist, searchMedicineByBrandName, updateMedicine, updatePharmacist } from '../controllers/pharmacists.Controller.js';
 const router = express.Router();
 
 
@@ -33,5 +33,14 @@ router.get('/getActivePatients', getAllActivePatients);
 
 // Get Patients Preescription
 router.get('/prescription/:patientId', getPatientsPrescription);
+
+// Get Medecine By Brand Name
+router.get('/searchBrand', searchMedicineByBrandName);
+
+// Add Medicine
+router.post('/addMedicine', addMedicine);
+
+// Update Medicine
+router.post('/addMedicine', updateMedicine);
 
 export default router;
